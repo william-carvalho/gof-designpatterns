491490cedadcf9bebdec4afab2f3ca1a820422c8
william-carvalho <will.carbas@gmail.com>
2026-08-10 12:45:30 -0300

    adapter

--- src/main/java/com/designpatterns/structural/adapter/Main.java
+++ src/main/java/com/designpatterns/structural/adapter/Main.java
@@ -3,6 +3,7 @@ package com.designpatterns.structural.adapter;
 public class Main {
 
     public static void main(String[] args) {
+        
         LegacyPaymentGateway legacyGateway = new LegacyPaymentGateway();
         PaymentProcessor paymentProcessor =
                 new PaymentGatewayAdapter(legacyGateway);
